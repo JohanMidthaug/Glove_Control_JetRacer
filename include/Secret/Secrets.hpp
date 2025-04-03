9edda975f351d879b421e59dc52c233ded50c005
HermanGran <142888421+HermanGran@users.noreply.github.com>
2025-04-03 13:05:57 +0200

    Added password example

---
+++ include/Secret/Secrets.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Herman Hårstad Gran on 03/04/2025.
+//
+
+#ifndef MOM_GLOVE_SECRETS_HPP
+#define MOM_GLOVE_SECRETS_HPP
+
+extern const char* EAP_SSID;
+extern const char* EAP_IDENTITY;
+extern const char* EAP_USERNAME;
+extern const char* EAP_PASSWORD;
+
+#endif //MOM_GLOVE_SECRETS_HPP
---
+++ src/Secret/SecretsExample.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Herman Hårstad Gran on 03/04/2025.
+//
+// secrets.cpp
+#include <Secret/Secrets.hpp>
+
+// Copy this file and call it Secrets.hpp
+// Then Type in personal login credentials
+const char* EAP_SSID     = "eduroam";
+const char* EAP_IDENTITY = "Username@stud.ntnu.no";
+const char* EAP_USERNAME = "Username";
+const char* EAP_PASSWORD = "****";  // Replace with the actual password
